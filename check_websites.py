219b066b941af4ff1e8b2ab90a26c83d99009721
MathewBiddle <mathew.biddle@gmail.com>
2025-03-20 22:57:05 -0400

    fix bug in website check

--- check_websites.py
+++ check_websites.py
@@ -42,10 +42,11 @@ def check_ftp_status(host):
 
 # Function to check URL availability
 def check_url(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
     if url.startswith("ftp"):
         return check_ftp_status(url)#np.nan
     try:
-        response = requests.head(url, allow_redirects=True, timeout=20)
+        response = requests.head(url, allow_redirects=True, timeout=20, headers=headers)
         return f'{response.status_code}'
     except RequestException as e:
         return f'{e}'
